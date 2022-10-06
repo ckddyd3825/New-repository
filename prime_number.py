fc961e43f35d93b09cf3a6d86fcb8a34989f94af
ckddyd3825 <ckddyd0977@naver.com>
2022-10-06 19:25:42 +0900

    Theropy Files

--- prime_number.py
+++ prime_number.py
@@ -3,5 +3,8 @@ def is_prime_number(x):
         if x % num == 0:
             print("The number {num1} is not a prime number.".format(num1=x))
             return
-    print("The number {num1} is a prime number.".format(num1=x))
 
+#wjdtmdghkszhstjxm
+#rkqhrhtlvek
+#tjdtlrudzhstjxmeh
+#rkqhrhtlvek
