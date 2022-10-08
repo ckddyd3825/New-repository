2cde078796307c73d95fad2d9ee802c96deb1c23
ckddyd3825 <112681594+ckddyd3825@users.noreply.github.com>
2022-10-08 09:34:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import prime_number as pn
 if __name__ == '__main__':
     pn.is_prime_number(1)
     pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+#define
+#전처리문
+#include
 
