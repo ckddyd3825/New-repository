00c86f23cd8e962bc8df1be446f6474eef625601
ckddyd3825 <ckddyd0977@naver.com>
2022-10-11 21:11:24 +0900

    New Codes

---
+++ Pink Venom.py
@@ -0,0 +1,4 @@
+#This that pink Venom
+#get'em get'em get'em
+#This that don't like wah wah wah
+#taske don't like ah ah ah
---
+++ Shut Down.py
@@ -0,0 +1,3 @@
+#Let me pull up me pull up me Shut Down
+#간판 내리고 문잠궈 Shut Down
+#Whip it Whip it Whip it
